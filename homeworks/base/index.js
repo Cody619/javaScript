29585e52228246581fe029d0f7198ed925908b92
Дмитрий Поволяев <dmitrijpovolaev@Cody.local>
2023-11-24 14:08:13 +0200

    home

---
+++ homeworks/base/index.js
@@ -0,0 +1,6 @@
+let name = prompt("what is your name?");
+let age = prompt("what is your age?");
+let email = prompt("whant is your email?");
+let sex = prompt("what is your sex?");
+
+console.log(name, age, email, sex);
