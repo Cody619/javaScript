6d10918d3ef7f0b4ac501f369a04de8d1401ccee
Дмитрий Поволяев <dmitrijpovolaev@Cody.local>
2023-11-29 23:06:58 +0200

    homwork1

--- homeworks/base/index.js
+++ homeworks/base/index.js
@@ -1,6 +1,27 @@
+//запитуємо іформацію юзера
 let userName = prompt("what is your name?");
 let age = prompt("what is your age?");
 let email = prompt("whant is your email?");
 let sex = prompt("what is your sex?");
+//виводимо в консоль данні юзера
+console.log(`ваші данні , ${userName} , ${age} ,${email} , ${sex}`);
 
-console.log(userName, age, email, sex);
+//створюємо поточний рік
+const years = 2023;
+const birsday = prompt("рік вашого народження");
+const count = years - birsday;
+
+//виводимо вік
+console.log(count);
+
+//робимо конвертор валюти
+
+//доллор до євро
+const dollor = 1.1;
+const cash = prompt("введіть вашу сумму");
+
+//робимо перевод
+const conclusion = dollor * cash;
+
+//виводимо конвертацію
+console.log(conclusion);
